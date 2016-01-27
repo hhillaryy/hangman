194f8aed68ef5b0a80a30389fce178b622bc5c74
Hillary Hintzen and Mao Thygesen <hillary07@gmail.com>
2016-01-27 11:33:10 -0800

    add first two specs, wordArray and hangmanWord

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-function hangmanWord (word, length) {
-  this.word = word;
-  this.length = length;
-  this.letters = [];
+var wordArray = function(word) {
+  var emptyArray = [];
+  for(var index = 0; index < word.length; index ++) {
+    emptyArray.push(word.slice(index, index + 1));
+  }
+return emptyArray;
 }
+
+function hangmanWord (word, length, letters) {
+  this.word = word;
+  this.length = word.length;
+  this.letters = wordArray(word);
+}
+
+//
+// hangmanWord.prototype.userLetter = function() {
+//
+//   for(var index = 0; index <= word.length; index += 1) {
+//     if(index === this.userLetter) {
+//       return "e";
+//     } else {
+//       return "x";
+//     };
+//   }
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,20 @@
-describe("hangmanWord", function(){
-  it("will create object with these specifications", function(){
-    var testWord = new hangmanWord ("hello", 4);
-    expect(testWord.word).to.equal("hello");
-    expect(testWord.length).to.equal(4);
-    expect(testWord.letters).to.eql([]);
+describe("wordArray", function (){
+  it("will make a word into an array", function(){
+    var testArray = new wordArray("hello");
+    expect(testArray).to.eql(["h", "e", "l", "l", "o"]);
   });
 });
+
+describe("hangmanWord", function(){
+  it("will create object with these specifications", function(){
+    var testWord = new hangmanWord ("hello", 5, ["h", "e", "l", "l", "o"]);
+    expect(testWord.word).to.equal("hello");
+    expect(testWord.length).to.equal(5);
+    expect(testWord.letters).to.eql(["h", "e", "l", "l", "o"]);
+  });
+});
+
+
+  // it("will select letter chosen by user", function (){
+  //   var testWord = new hangmanWord ("hello", ["h", "e", "l", "l", "o"]);
+  //   expect(testword.userLetter).to.equal("e");
